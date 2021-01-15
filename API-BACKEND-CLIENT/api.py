e4fe2d2e83bce38dd1506f1712c839082d0de581
Nicolas Schüssling <nicolas.schuessling@gmx.at>
2021-01-15 16:42:04 +0100

    Update api.py

--- API-BACKEND-CLIENT/api.py
+++ API-BACKEND-CLIENT/api.py
@@ -136,10 +136,6 @@ class Playback(Resource):
 
 api.add_resource(Playback, conf_client_backend.path)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-
 
 if __name__ == '__main__':
     from gevent import monkey
