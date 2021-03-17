d7ec2afee575446f7e10efbf0a18e20b8618d444
Nicolas Schüssling <nicolas.schuessling@gmx.at>
2021-03-17 16:31:45 +0100

    updated logging

--- API/req.py
+++ API/req.py
@@ -61,7 +61,7 @@ def greq_post(urls):
     req_url = []
     for url in urls:
         req_url.append(grequests.get(url, timeout=2))
-    print(urls)  # TODO: Logging
+    print(urls)
     res = grequests.map(req_url)
     return res
 
